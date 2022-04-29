5a17ce31c3cc2c9d1629d07de503d5259cbdfa3c
adamwojdan <wojdan.adam@gmail.com>
2022-04-29 07:10:17 +0200

    refactor and add timer for waitForPageLoad()

--- src/main/java/HelloSelenium.java
+++ src/main/java/HelloSelenium.java
@@ -25,12 +25,6 @@ public class HelloSelenium {
         ChromeOptions options = new ChromeOptions();
         System.setProperty("webdriver.chrome.driver", "C:\\BrowserDrivers\\chromedriver.exe");
 
-//        FirefoxOptions options = new FirefoxOptions();
-//        options.setBinary("C:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe"); //This is the location where you have installed Firefox on your machine
-//        System.setProperty("webdriver.gecko.driver", "C:\\BrowserDrivers\\geckodriver.exe");
-//        FirefoxDriver driver = new FirefoxDriver(options);
-//        FirefoxDriver driver = new FirefoxDriver();
-
         ChromeDriver driver = new ChromeDriver(options);
 //        driver.manage().window().minimize();
 
@@ -38,49 +32,23 @@ public class HelloSelenium {
         driver.get("https://fakemail.adamwojdan.pl/");
 
 
-        sendKeysLikeHuman(driver, TypeToFind.NAME, "name", "Twój Bank Internetowy");
-        randomDelayLikeHuman();
-
-        sendKeysLikeHuman(driver, TypeToFind.NAME, "email", "info@twojbank.pl");
-        randomDelayLikeHuman();
-
-        sendKeysLikeHuman(driver, TypeToFind.NAME, "toEmail", "wojdan.adam@gmail.com");
-        randomDelayLikeHuman();
-
-        sendKeysLikeHuman(driver, TypeToFind.NAME, "subject", "Zmiana regulaminu");
-        randomDelayLikeHuman();
-
-        sendKeysLikeHuman(driver, TypeToFind.NAME, "message", "Nowy regulamin jest pod tym linkiem: towcaleniefake.cc/1ryu9wdh1789tg89ighd19");
-        randomDelayLikeHuman();
+        typeKeysLikeHuman(driver, TypeToFind.NAME, "name", "Twój Bank Internetowy");
+//        sendKeysLikeHuman(driver, TypeToFind.NAME, "email", "info@twojbank.pl");
+//        sendKeysLikeHuman(driver, TypeToFind.NAME, "toEmail", "wojdan.adam@gmail.com");
+//        sendKeysLikeHuman(driver, TypeToFind.NAME, "subject", "Zmiana regulaminu");
+//        sendKeysLikeHuman(driver, TypeToFind.NAME, "message", "Nowy regulamin jest pod tym linkiem: towcaleniefake.cc/1ryu9wdh1789tg89ighd19");
 
         Select dropdown = new Select(driver.findElement(By.id("typeOfContent")));
         dropdown.selectByIndex(1);
 
         randomDelayLikeHuman();
-        randomDelayLikeHuman();
 
-
-        driver.get("https://projekty.adamwojdan.pl");
+        driver.get("https://lesiogotuje.pl/");
         waitForPageLoad(driver);
-
-
-//        driver.get("https://adamwojdan.pl/wp-login.php?redirect_to=https%3A%2F%2Fadamwojdan.pl%2Fwp-admin%2F&reauth=1");
-//        Thread.sleep(500);
-//        driver.findElement(By.name("name")).sendKeys("PKO Bank Polski");
-//        Thread.sleep(500);
-//        driver.findElement(By.name("email")).sendKeys("info@ipko.pl");
-//        Thread.sleep(500);
-//        WebElement recaptcha = driver.findElement(By.id("wp-submit"));
-//        recaptcha.click();
-
-//        Thread.sleep(2500);
-//
-//        driver.get("https://adamwojdan.pl/wp-admin/post-new.php?post_type=page");
-
     }
 
 
-    public static void sendKeysLikeHuman(ChromeDriver driver, TypeToFind type, String fieldName, String textToWrite) throws InterruptedException {
+    public static void typeKeysLikeHuman(ChromeDriver driver, TypeToFind type, String fieldName, String textToWrite) throws InterruptedException {
 
         Random random = new Random();
 
@@ -93,10 +61,10 @@ public class HelloSelenium {
 
             String alphabet = "qwertyuiop[]asdfghjkl;'zxcvbnm,.1234567890-=";
 
-            if (random.nextInt(0,100) < 10) {
+            if (random.nextInt(0, 100) < 2) {
 
-                String randomText = "" + alphabet.charAt(random.nextInt(0,alphabet.length()))
-                        + alphabet.charAt(random.nextInt(0,alphabet.length()));
+                String randomText = "" + alphabet.charAt(random.nextInt(0, alphabet.length()))
+                        + alphabet.charAt(random.nextInt(0, alphabet.length()));
 
                 switch (type) {
                     case CLASS -> driver.findElement(By.className(fieldName)).sendKeys(randomText);
@@ -128,24 +96,26 @@ public class HelloSelenium {
             }
 
             Thread.sleep(100 + random.nextInt(-95, 150));
-            System.out.println(100 + random.nextInt(-95, 150));
+            System.out.println("Random delay for type key: " + 100 + random.nextInt(-95, 150));
         }
+        randomDelayLikeHuman();
     }
 
     public static void randomDelayLikeHuman() throws InterruptedException {
         Random random = new Random();
-        Thread.sleep(500 + random.nextInt(-250, 750));
+        Thread.sleep(random.nextInt(250, 1000));
     }
 
+    public static void waitForPageLoad(ChromeDriver driver) throws InterruptedException {
 
-    public static void waitForPageLoad(ChromeDriver driver) {
-
+        long currentTime = System.currentTimeMillis();
         do {
             js = (JavascriptExecutor) driver;
             pageLoadStatus = (String) js.executeScript("return document.readyState");
         } while (!pageLoadStatus.equals("complete"));
 
-        System.out.println("Page Loaded.");
+        long pageLoadTime = System.currentTimeMillis() - currentTime;
+        System.out.println("Page Loaded in " + pageLoadTime + " ms");
 
     }
 
